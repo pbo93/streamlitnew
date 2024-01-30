d115231b59c7216e7e32c12f1b74112a7db20a62
pbo93 <57297737+pbo93@users.noreply.github.com>
2024-01-30 12:11:38 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,5 +1,10 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import tensorflow as tf
 
 st.title('FIAP LUB')
+
+with st.container():
+  st.subheader("Deploy do Método de :blue[Rede Neurais Recorrentes]")
+  st.write("Abaixo é possivel realizar a previsão do preço do petróleo wit selecionando a data desejada e clicando no botão Fazer Previsão")
